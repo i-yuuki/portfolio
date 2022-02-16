df596b54754ce56f6189fd491542a7e32e6276fd
i-yuuki <i-yuuki@users.noreply.github.com>
2022-02-16 16:29:48 +0900

    トップページ動画にplaysInlineを追加
    謎に iOS Safari 対応

--- components/hero.js
+++ components/hero.js
@@ -4,7 +4,7 @@ export default function Hero(props) {
   return (
     <section className={styles['pf-hero']}>
       {props.background && <div className={styles['pf-hero-background']} style={{backgroundImage: `url(${props.background})`}}></div>}
-      {props.video && <video muted autoPlay loop src={props.video} className={styles['pf-hero-background']}></video>}
+      {props.video && <video muted autoPlay playsInline loop src={props.video} className={styles['pf-hero-background']}></video>}
       {props.children}
     </section>
   )
